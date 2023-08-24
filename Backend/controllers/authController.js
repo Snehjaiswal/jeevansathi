6f4d65857277e9a9ad319cfbb08892c05c503f29
Anmol <anmol06112002@gmail.com.com>
2023-08-24 17:27:04 +0530

    massage

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -27,7 +27,7 @@ async function signUp(req, res) {
       candidates_name,
       surname,
       email,
-      password:hashedPassword,
+      password: hashedPassword,
       number,
       work,
       gendar,
@@ -77,27 +77,29 @@ async function login(req, res) {
 
     // Find user by email
 
-    console.log(email,password);
+    console.log(email, password);
     const user = await User.findOne({ email });
-
+    if (!user) {
+      return res.send({message:"user is not exist"})
+    }
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    console.log("ooo",password,isPasswordValid);
+    console.log("ooo", password, isPasswordValid);
     if (!isPasswordValid || !user) {
       return res.status(401).json({ message: 'Invalid email or password.' });
     }
     const perenst = await Perents.findOne({ user_id: user._id })
     const about = await About.findOne({ user_id: user._id })
-   
+
 
     // Create and send JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: '10d',
     });
 
-    res.status(200).json({  message: 'Login successful',token, user, perenst, about });
+    res.status(200).json({ message: 'Login successful', token, user, perenst, about });
   } catch (error) {
     console.error('Error during login:', error);
-    console.log(error,'-------');
+    console.log(error, '-------');
     res.status(500).json({ message: 'An error occurred during login.' });
   }
 };
