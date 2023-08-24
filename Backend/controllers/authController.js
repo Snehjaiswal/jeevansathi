456be569588610fe4f9f741b17befa8c304a07cc
Anmol <anmol06112002@gmail.com.com>
2023-08-24 17:12:23 +0530

    login-api

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,11 +1,18 @@
 const bcrypt = require('bcrypt');
-const { User, Perents } = require('../models/User.model');
+const { User, Perents, About } = require('../models/User.model');
 const jwt = require('jsonwebtoken');
 
 async function signUp(req, res) {
   try {
-    const { candidates_name, surname, email ,number,work,gendar,loking} = req.body;
-    const {user_id,mother_name,father_name,gotra ,father_occupation,mother_occupation,sister,brother,status,city,native_city,address} = req.body
+    const { candidates_name, surname, email, number, work, gendar, loking, password } = req.body;
+    const { user_id, mother_name, father_name, gotra, father_occupation, mother_occupation, sister, brother, status, city, native_city, address } = req.body
+    const { height,
+      education,
+      family_type,
+      professional,
+      physically_challenge,
+      about_your_future_carrer,
+      picture } = req.body
     // Check if the username or email is already registered
     const existingUser = await User.findOne({ $or: [{ candidates_name }, { email }] });
     if (existingUser) {
@@ -13,23 +20,24 @@ async function signUp(req, res) {
     }
 
     // Hash the password
-    // const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create a new user
     const newUser = new User({
       candidates_name,
       surname,
       email,
+      password:hashedPassword,
       number,
       work,
       gendar,
       loking,
-      type:1
+
     });
 
     await newUser.save();
     const perents_info = new Perents({
-      user_id  ,
+      user_id: newUser._id,
       mother_name,
       father_name,
       gotra,
@@ -41,9 +49,21 @@ async function signUp(req, res) {
       city,
       native_city,
       address,
-      type:2
+
     })
     await perents_info.save()
+    const about_bio = new About({
+      user_id: newUser._id,
+      height,
+      education,
+      family_type,
+      professional,
+      physically_challenge,
+      about_your_future_carrer,
+      picture,
+
+    })
+    await about_bio.save()
     res.status(201).json({ message: 'User registered successfully.' });
   } catch (error) {
     console.error('Error registering user:', error);
@@ -56,24 +76,28 @@ async function login(req, res) {
     const { email, password } = req.body;
 
     // Find user by email
+
+    console.log(email,password);
     const user = await User.findOne({ email });
-    // if (!user) {
-    //   return res.status(401).json({ message: 'Invalid email or password.' });
-    // }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log("ooo",password,isPasswordValid);
     if (!isPasswordValid || !user) {
       return res.status(401).json({ message: 'Invalid email or password.' });
     }
+    const perenst = await Perents.findOne({ user_id: user._id })
+    const about = await About.findOne({ user_id: user._id })
+   
 
     // Create and send JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: '10d',
     });
 
-    res.status(200).json({ token });
+    res.status(200).json({  message: 'Login successful',token, user, perenst, about });
   } catch (error) {
     console.error('Error during login:', error);
+    console.log(error,'-------');
     res.status(500).json({ message: 'An error occurred during login.' });
   }
 };
--- Backend/models/User.model.js
+++ Backend/models/User.model.js
@@ -4,11 +4,13 @@ const userSchema = new mongoose.Schema({
   candidates_name: { type: String, required: true, },
   surname: { type: String, required: true, },
   email: { type: String, required: true },
+  password:{type:String},
   number: { type: Number },
   work: { type: String },
   gendar: { type: String },
   loking: { type: String },
-  // Additional fields can be added here
+  status_type: { type: Number, default: 1 }
+
 });
 const userPerenstSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
@@ -23,10 +25,25 @@ const userPerenstSchema = new mongoose.Schema({
   city: { type: String },
   native_city: { type: String },
   address: { type: String },
-  // Additional fields can be added here
+  status_type: { type: Number, default: 2 }  
+
 });
 
+const about_bio = new mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
+  height: { type: String },
+  education: { type: String },
+  family_type: { type: String },
+  professional: { type: String },
+  physically_challenge: { type: String },
+  about_your_future_carrer: { type: String },
+  picture: { type: String },
+  status_type: { type: Number, default: 3 }  
+
+})
 const Perents = mongoose.model('perents', userPerenstSchema)
 const User = mongoose.model('User', userSchema);
+const About=mongoose.model('about',about_bio)
 
-module.exports = { User, Perents };
+module.exports = { User, Perents ,About};
